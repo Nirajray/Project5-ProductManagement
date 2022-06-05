015e0dd4283da06d546c874c0f2f0a8d39c8a3e4
Nirajray <nirajray5555@gmail.com>
2022-06-05 19:55:58 +0530

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json()); //transforms the text-based JSON input into JS-acces
 
 
 mongoose.connect(
-        "mongodb+srv://Nirajkumar:2gkjm25Aa8wh01yS@cluster0.kzih8.mongodb.net/group21Database?authSource=admin&replicaSet=atlas-11shqf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
+        "mongodb+srv://Nirajkumar:2gkjm25Aa8wh01yS@cluster0.kzih8.mongodb.net/Project-5?authSource=admin&replicaSet=atlas-11shqf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
             useNewUrlParser: true,
         }
     )
